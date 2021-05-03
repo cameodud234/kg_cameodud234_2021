214a802741ec05949e01a7105c8e68ab8da879f7
sunniestAcorn00 <30845397+cameodud234@users.noreply.github.com>
2021-05-03 15:41:21 -0400

    preliminary code

---
+++ main.js
@@ -0,0 +1,16 @@
+let test1 = [30,-4,5,7,909];
+
+// let test2 = [-3,-4,5,7,9];
+
+let numsDict = {
+    0: "Zero",
+    1: "One",
+    2: "Two",
+    3: "Three",
+    4: "Four",
+    5: "Five",
+    6: "Six",
+    7: "Seven",
+    8: "Eight",
+    9: "Nine"
+};
